1618adc394d03a05029af1fd72cebd141ed40ee9
Mohammed Tanvir <tanvir.eatl@gmail.com>
2026-05-16 12:52:50 +0600

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
@@ -15,10 +14,10 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+        <Stack.Screen name="index" options={{
+          headerShown: false
+        }} />
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import "@/global.css";
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text className="text-center text-blue-500">Home Screen</Text>
+        </View>
+    )
+}
