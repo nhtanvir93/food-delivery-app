ac2c3d64e4b8d1e6e5998d38f3cbaff86c891b17
Mohammed Tanvir <tanvir.eatl@gmail.com>
2026-05-21 22:20:18 +0600

    Feat: search btn added

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,26 @@
+import { useRef } from "react";
 import { View, Text } from "react-native";
 
 import Screen from "@/components/Screen";
-import SearchInput from "@/components/search-input";
+import SearchInput, { type SearchInputType } from "@/components/search-input";
+import { Button } from "@/components/ui/button";
 
 export default function HomeScreen() {
+  const searchInputRef = useRef<SearchInputType>(null);
+
   return (
     <Screen>
       <View className="gap-2">
         <Text className="text-xl font-bold tracking-wide text-foreground">
           What would you like to eat?
         </Text>
-        <SearchInput />
+        <SearchInput ref={searchInputRef} />
+        <Button
+          onPress={() => console.log(searchInputRef?.current?.getValue())}
+          variant="outline"
+        >
+          <Text className="text-foreground">Search</Text>
+        </Button>
       </View>
     </Screen>
   );
--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,23 +1,46 @@
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { memo, useState } from "react";
+import {
+  forwardRef,
+  memo,
+  type Ref,
+  useImperativeHandle,
+  useState,
+} from "react";
 import { View, TextInput } from "react-native";
 
-const SearchInput = ({
-  placeholder = "Type here...",
-}: {
-  placeholder?: string;
-}) => {
+export interface SearchInputType {
+  getValue: () => string;
+  clear: () => void;
+}
+
+const SearchInput = (
+  {
+    placeholder = "Type here...",
+  }: {
+    placeholder?: string;
+  },
+  ref: Ref<SearchInputType>,
+) => {
   const [searchKey, setSearchKey] = useState("");
 
+  useImperativeHandle(
+    ref,
+    () => ({
+      getValue: () => searchKey,
+      clear: () => setSearchKey(""),
+    }),
+    [searchKey],
+  );
+
   return (
-    <View className="flex-row items-center gap-1 rounded-xl border border-border bg-input px-3 py-1">
+    <View className="flex-row items-center gap-1 rounded-xl border border-border bg-input px-3">
       <Ionicons
         className="text-muted-foreground"
         name="search-outline"
         size={24}
       />
       <TextInput
-        className="flex-1 text-lg text-foreground"
+        className="flex-1 text-black"
         placeholder={placeholder}
         onChangeText={setSearchKey}
       />
@@ -25,4 +48,4 @@ const SearchInput = ({
   );
 };
 
-export default memo(SearchInput);
+export default memo(forwardRef(SearchInput));
