290fe3146ff6bfcf6961700f0dad7e218516866c
Mohammed Tanvir <tanvir.eatl@gmail.com>
2026-05-21 15:54:49 +0600

    Fix: header component added in the screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,10 @@
 import { Text } from "react-native";
 
-import Header from "@/components/header";
 import Screen from "@/components/Screen";
 
 export default function HomeScreen() {
   return (
-    <Screen header=<Header />>
+    <Screen>
       <Text>Home</Text>
     </Screen>
   );
--- components/Screen.tsx
+++ components/Screen.tsx
@@ -2,18 +2,19 @@ import { type ReactNode } from "react";
 import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import Header from "./header";
+
 interface Props {
-  header: ReactNode;
   children: ReactNode;
 }
 
-export default function Screen({ header, children: content }: Props) {
+export default function Screen({ children: content }: Props) {
   return (
     <SafeAreaView
       edges={["top", "bottom"]}
       className="relative flex-1 bg-background"
     >
-      {header}
+      <Header />
       <View className="flex-1 p-4">{content}</View>
     </SafeAreaView>
   );
