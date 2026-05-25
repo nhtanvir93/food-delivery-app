477374cbf95bdcc4ea402b7e927da601a19750db
Mohammed Tanvir <tanvir.eatl@gmail.com>
2026-05-25 23:38:07 +0600

    Fix: clear btn added to clear the search input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -6,7 +6,10 @@ import {
   useImperativeHandle,
   useState,
 } from "react";
-import { View, TextInput } from "react-native";
+import { View, TextInput, Pressable } from "react-native";
+
+import { COLORS } from "@/constants/theme";
+import { useColorScheme } from "@/lib/useColorScheme";
 
 export interface SearchInputType {
   getValue: () => string;
@@ -22,6 +25,9 @@ const SearchInput = (
   ref: Ref<SearchInputType>,
 ) => {
   const [searchKey, setSearchKey] = useState("");
+
+  const { colorScheme } = useColorScheme();
+  const theme = colorScheme === "dark" ? COLORS.dark : COLORS.light;
 
   useImperativeHandle(
     ref,
@@ -41,9 +47,19 @@ const SearchInput = (
       />
       <TextInput
         className="flex-1 tracking-wide text-black"
+        value={searchKey}
         placeholder={placeholder}
         onChangeText={setSearchKey}
       />
+      {searchKey.length > 0 && (
+        <Pressable onPress={() => setSearchKey("")}>
+          <Ionicons
+            name="close-circle-outline"
+            size={24}
+            color={theme.destructive}
+          />
+        </Pressable>
+      )}
     </View>
   );
 };
--- constants/theme.ts
+++ constants/theme.ts
@@ -1,42 +1,37 @@
 export const COLORS = {
   light: {
     background: "#FFFFFF",
-
     primary: "#0A0A0A",
     primaryForeground: "#FFFFFF",
-
     secondary: "#F5F5F5",
     secondaryForeground: "#FFFFFF",
-
-    destructive: "#E14B4B",
+    destructive: "#DC3535",
     destructiveForeground: "#FFFFFF",
-
-    accent: "#297DFF",
+    accent: "#2979FF",
     accentForeground: "#FFFFFF",
-
     textForeground: "#0A0A0A",
-    textMutedForeground: "#B4B4BC",
-
-    iconBackground: "#E8ECF1",
-    input: "#F3F4F6",
-
-    star: "#F0B000",
+    textMutedForeground: "#B3B3BF",
+    iconBackground: "#E0E3EA",
+    input: "#F2F2F7",
+    star: "#F0AE00",
+    border: "#D7D7D7",
   },
-
   dark: {
     background: "#0A0A0A",
-
-    primary: "#FFFFFF",
-    primaryForeground: "#0A0A0A",
-
+    primary: "#262626",
+    primaryForeground: "#FFFFFF",
     secondary: "#262626",
     secondaryForeground: "#0A0A0A",
-
+    destructive: "#DC3535",
+    destructiveForeground: "#FFFFFF",
+    accent: "#2979FF",
+    accentForeground: "#FFFFFF",
     textForeground: "#FAFAFA",
     textMutedForeground: "#969696",
-
     iconBackground: "#262626",
     input: "#FFFFFF",
+    star: "#F0AE00",
+    border: "#323232",
   },
 } as const;
 
